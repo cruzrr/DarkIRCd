5c83e50f43ed650f181671404ec7f7720566208b
Aaron Blakely <aaron@ephasic.org>
2012-03-03 18:41:25 -0600

    added server parser struct

--- lib/parser.h
+++ lib/parser.h
@@ -1,12 +1,20 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct _s {
-    char *command;
-    char *prefix;
-    char *postfix;
-    int arg_count;
+typedef struct _c
+{
+	char *command;
+	char *prefix;
+	char *postfix;
+	int arg_count;
 } ircClientRaw;
 
+typedef struct _s
+{
+	char *command;
+	char *prefix;
+	char *postfix;
+	int arg_count;
+} ircServerRaw;
 
 #endif
