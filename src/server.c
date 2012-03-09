21f87dd91b8c34d338015e73e41a2a76fb871ec6
Aaron Blakely <aaron@ephasic.org>
2012-03-08 18:52:58 -0600

    cleanup on server.c

--- src/server.c
+++ src/server.c
@@ -9,21 +9,6 @@
 
 #include "server.h"
 
-fd_set master;
-fd_set read_fds;
-struct sockaddr_in serveraddr;
-struct sockaddr_in clientaddr;
-
-int fdmax;
-int listener;
-int newfd;
-
-char buf[1024];
-int nbytes;
-int yes = 1;
-int addrlen;
-int i, j;
-
 int init_server(int port)
 {
 
