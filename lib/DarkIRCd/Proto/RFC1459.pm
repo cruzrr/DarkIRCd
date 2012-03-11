1d2173f13d3189031e6614a05dae7d5c7aee8a8b
Aaron Blakely <aaron@ephasic.org>
2012-03-11 14:47:39 -0500

    started on TS6.pm

--- lib/DarkIRCd/Proto/RFC1459.pm
+++ lib/DarkIRCd/Proto/RFC1459.pm
@@ -5,6 +5,20 @@ package DarkIRCd::Proto::RFC1459;
 #
 # Copyright (C) 2012 cruzrr
 # Website: http://www.cruzrr.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 use strict;
 use warnings;
@@ -19,47 +33,47 @@ sub new {
 sub proto_def {
 	my $class = shift;
 	my $proto = (
-		WELCOME		=> '001',
-		SERV_HOST	=> '002',
-		SERV_CRET	=> '003',
-		SERV_LINF	=> '004',
-		SERV_INFO	=> '005',
+		WELCOME     => '001',
+		SERV_HOST   => '002',
+		SERV_CRET   => '003',
+		SERV_LINF   => '004',
+		SERV_INFO   => '005',
 		
-		AWAY		=> '301',
-		UNAWAY		=> '305',
-		NOWAWAY		=> '306',
-		WHOIS_USER	=> '311',
-		WHOIS_SERV	=> '312',
-		WHOIS_OPER	=> '313',
-		WHOWAS_USR	=> '314',
-		WHOIS_IDLE	=> '317',
-		WHOIS_END	=> '318',
-		WHOIS_CHAN	=> '319',
+		AWAY        => '301',
+		UNAWAY      => '305',
+		NOWAWAY     => '306',
+		WHOIS_USER  => '311',
+		WHOIS_SERV  => '312',
+		WHOIS_OPER  => '313',
+		WHOWAS_USR  => '314',
+		WHOIS_IDLE  => '317',
+		WHOIS_END   => '318',
+		WHOIS_CHAN  => '319',
 		
-		TOPIC		=> '332',
-		NOTOPIC		=> '331',
-		INVITING	=> '341',
-		NAME_REPLY	=> '353',
-		NAME_END	=> '366',
-		WHOWAS_END	=> '369',
+		TOPIC       => '332',
+		NOTOPIC     => '331',
+		INVITING    => '341',
+		NAME_REPLY  => '353',
+		NAME_END    => '366',
+		WHOWAS_END  => '369',
 		
-		LIST_START	=> '321',
-		LIST		=> '322',
-		LIST_END	=> '323',
+		LIST_START  => '321',
+		LIST        => '322',
+		LIST_END    => '323',
 		
-		VERSION		=> '351',
-		MOTD_START	=> '375',
-		MOTD		=> '372',
-		MOTD_END	=> '376',
-		WHO			=> '352',
-		WHO_END		=> '315',
+		VERSION     => '351',
+		MOTD_START  => '375',
+		MOTD        => '372',
+		MOTD_END    => '376',
+		WHO         => '352',
+		WHO_END     => '315',
 		
-		CHAN_MODES	=> '324',
-		BAN_LIST	=> '367',
-		BAN_END		=> '368',
+		CHAN_MODES  => '324',
+		BAN_LIST    => '367',
+		BAN_END     => '368',
 		
-		USER_ISOP	=> '381',
-		TIME		=> '391'
+		USER_ISOP   => '381',
+		TIME        => '391'
 	);
 
 	return $proto;
@@ -68,31 +82,31 @@ sub proto_def {
 sub error_def {
 	my $class = shift;
 	my $errors = (
-		NOSUCHNICK	=> '401',
-		NOSUCHSERV	=> '402',
-		CANNOTSEND	=> '404',
-		WASNONICK	=> '406',
-		NORECIPIENT	=> '411',
-		NOTEXTTOSND	=> '412',
+		NOSUCHNICK  => '401',
+		NOSUCHSERV  => '402',
+		CANNOTSEND  => '404',
+		WASNONICK   => '406',
+		NORECIPIENT => '411',
+		NOTEXTTOSND => '412',
 		NOGIVENNICK => '431',
-		BADNICK		=> '432',
-		NAMEINUSE	=> '433',
+		BADNICK     => '432',
+		NAMEINUSE   => '433',
 		USRNOTTHERE => '441',
-		USERONCHAN	=> '443',
-		NOSUCHCHAN	=> '403',
-		NOTONCHAN	=> '442',
-		NEEDMOREPRM	=> '461',
-		WRONGPASS	=> '464',
+		USERONCHAN  => '443',
+		NOSUCHCHAN  => '403',
+		NOTONCHAN   => '442',
+		NEEDMOREPRM => '461',
+		WRONGPASS   => '464',
 		YOUAREBANND => '465',
-		KEYSET		=> '467',
-		CHANISFULL	=> '471',
-		INVITEONLY	=> '473',
-		BANNED		=> '474',
-		BADCHANKEY	=> '475',
-		NOPRIVS		=> '481',
-		CHANOPSREQ	=> '482',
-		NOOPERHOST	=> '491',
-		UNLIKEUSER	=> '502'
+		KEYSET      => '467',
+		CHANISFULL  => '471',
+		INVITEONLY  => '473',
+		BANNED      => '474',
+		BADCHANKEY  => '475',
+		NOPRIVS     => '481',
+		CHANOPSREQ  => '482',
+		NOOPERHOST  => '491',
+		UNLIKEUSER  => '502'
 	);
 
 	return $errors;
