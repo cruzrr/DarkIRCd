ef77f153e7244a207d9abf9c393c35d3d49e6034
Aaron Blakely <aaron@ephasic.org>
2012-03-10 23:44:59 -0600

    perl.

---
+++ src/ircd.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+
+# DarkIRCd - Modular and Lightweight IRCd in Perl.
+#
+# Copyright (C) 2012 cruzrr
+# Website: http://www.cruzrr.com
+
+use strict;
+use warnings;
+
+# erm, well... nothing to do yet. :P
