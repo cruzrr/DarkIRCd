b468063b504250fc775c57ea437acd9a0d731ccd
Aaron Blakely <aaron@ephasic.org>
2012-03-11 14:36:24 -0500

    finished rfc1459.pm

--- lib/DarkIRCd/Proto/RFC1459.pm
+++ lib/DarkIRCd/Proto/RFC1459.pm
@@ -16,7 +16,7 @@ sub new {
 	return bless($self, $class);
 }
 
-sub def {
+sub proto_def {
 	my $class = shift;
 	my $proto = (
 		WELCOME		=> '001',
@@ -30,10 +30,102 @@ sub def {
 		NOWAWAY		=> '306',
 		WHOIS_USER	=> '311',
 		WHOIS_SERV	=> '312',
+		WHOIS_OPER	=> '313',
+		WHOWAS_USR	=> '314',
+		WHOIS_IDLE	=> '317',
+		WHOIS_END	=> '318',
+		WHOIS_CHAN	=> '319',
 		
+		TOPIC		=> '332',
+		NOTOPIC		=> '331',
+		INVITING	=> '341',
+		NAME_REPLY	=> '353',
+		NAME_END	=> '366',
+		WHOWAS_END	=> '369',
+		
+		LIST_START	=> '321',
+		LIST		=> '322',
+		LIST_END	=> '323',
+		
+		VERSION		=> '351',
+		MOTD_START	=> '375',
+		MOTD		=> '372',
+		MOTD_END	=> '376',
+		WHO			=> '352',
+		WHO_END		=> '315',
+		
+		CHAN_MODES	=> '324',
+		BAN_LIST	=> '367',
+		BAN_END		=> '368',
+		
+		USER_ISOP	=> '381',
+		TIME		=> '391'
 	);
 
 	return $proto;
+}
+
+sub error_def {
+	my $class = shift;
+	my $errors = (
+		NOSUCHNICK	=> '401',
+		NOSUCHSERV	=> '402',
+		CANNOTSEND	=> '404',
+		WASNONICK	=> '406',
+		NORECIPIENT	=> '411',
+		NOTEXTTOSND	=> '412',
+		NOGIVENNICK => '431',
+		BADNICK		=> '432',
+		NAMEINUSE	=> '433',
+		USRNOTTHERE => '441',
+		USERONCHAN	=> '443',
+		NOSUCHCHAN	=> '403',
+		NOTONCHAN	=> '442',
+		NEEDMOREPRM	=> '461',
+		WRONGPASS	=> '464',
+		YOUAREBANND => '465',
+		KEYSET		=> '467',
+		CHANISFULL	=> '471',
+		INVITEONLY	=> '473',
+		BANNED		=> '474',
+		BADCHANKEY	=> '475',
+		NOPRIVS		=> '481',
+		CHANOPSREQ	=> '482',
+		NOOPERHOST	=> '491',
+		UNLIKEUSER	=> '502'
+	);
+
+	return $errors;
+}
+
+sub checknick {
+	my ($class, $nick) = @_;
+	
+	if ($nick =~ /^[^a-z]|[^\W_^`\\\[\]{}]/i) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+sub checkchan {
+	my ($class, $chan) = @_;
+	
+	if ($chan =~ /[\x00\x07\n\r ,]/) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+sub checkchankey {
+	my ($class, $key) = @_;
+	
+	if ($key =~ /[\x80-\uFFFF\x00\t\n\x0B\x0C\r ,]/) {
+		return 0;
+	} else {
+		return 1;
+	}
 }
 
 1;
